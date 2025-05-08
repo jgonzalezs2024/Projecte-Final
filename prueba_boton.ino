0e7d73b9bc3f87b8805a5110600ab650e44a0d22
Javi <jgonzalezs@insjoaquimmir.cat>
2025-05-08 15:56:11 +0200

    08052025

---
+++ prueba_boton.ino
@@ -0,0 +1,27 @@
+const int boton = 3;
+
+void setup() {
+  pinMode(boton, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // ... código previo
+
+  Serial.println("PUERTA ABIERTA");
+
+  bool puertaAbierta = true;
+
+  while (puertaAbierta) {
+    if (digitalRead(boton) == LOW) {
+      Serial.println("Botón presionado: cerrar puerta");
+      puertaAbierta = false;
+    }
+
+    delay(50);  // evita saturar el micro
+  }
+
+  // Aquí cierras la puerta
+  Serial.println("PUERTA CERRADA");
+  // Código para cerrar la puerta (relé, motor, etc.)
+}
